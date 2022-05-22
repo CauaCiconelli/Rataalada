7f062885dc466adc3f269f57acb0957dc7f50917
CauaCiconelli <caua.ciconelli@sptech.school>
2022-05-22 19:32:26 -0300

    Página de mais procurados

--- Rataalada/main/funcs/funcs.js
+++ Rataalada/main/funcs/funcs.js
@@ -1,5 +1,6 @@
 var entrada = []
 var userName = ''
+var page = 1
 function cClick() {
     entrada[0] = 'c'
     if (entrada[3] == 'r' && entrada[1] == 'h'&& entrada[2] == 'a'&& entrada[5] == 'd'&& entrada[4] == 'a'&& entrada[6] == 'a') {
@@ -59,4 +60,81 @@ function aClick3() {
 
 function charada(){
     window.location.replace('main/rataalada.html');
+}
+function usuario(){
+    user = userInp.value
+    if (user == `charada`){
+        alert(`charada?`)
+    }
+}
+
+function criminosoEsq(){
+    page--
+    if (page == 1) {
+        nomeCrime.innerHTML = `Nome: Victor Fries`
+        idadeCrime.innerHTML = `Idade: 47`
+        akaCrime.innerHTML = `Conhecido como: Senhor Frio`
+        procCrime.innerHTML = `Procurado desde: 01/05/2019`
+        criminoso.src = `main/imgs/fries.jpeg`
+    } else if (page == 2) {
+        nomeCrime.innerHTML = `Nome: Selina Kyle`
+        idadeCrime.innerHTML = `Idade: 28`
+        akaCrime.innerHTML = `Conhecida como: Mulher Gato`
+        procCrime.innerHTML = `Procurada desde: 17/09/2021`
+        criminoso.src = `main/imgs/selina.jpg`
+    } else if (page == 3) {
+        nomeCrime.innerHTML = `Nome: Bane Dorrance`
+        idadeCrime.innerHTML = `Idade: 30`
+        akaCrime.innerHTML = `Conhecido como: Bane`
+        procCrime.innerHTML = `Procurado desde: 21/11/2021`
+        criminoso.src = `main/imgs/bane.jpeg`
+    } else if (page == 4) {
+        nomeCrime.innerHTML = `Nome: Edward Nigma`
+        idadeCrime.innerHTML = `Idade: 34`
+        akaCrime.innerHTML = `Conhecido como: Charada`
+        procCrime.innerHTML = `Procurado desde: 21/11/2021`
+        criminoso.src = `main/imgs/nigma.jpg`
+    } else if (page == 0) {
+        page = 4
+        nomeCrime.innerHTML = `Nome: Edward Nigma`
+        idadeCrime.innerHTML = `Idade: 34`
+        akaCrime.innerHTML = `Conhecido como: Charada`
+        procCrime.innerHTML = `Procurado desde: 21/11/2021`
+        criminoso.src = `main/imgs/nigma.jpg`
+    }
+}
+
+function criminosoDir(){
+    page++
+    if (page == 1) {
+        nomeCrime.innerHTML = `Nome: Victor Fries`
+        idadeCrime.innerHTML = `Idade: 47`
+        akaCrime.innerHTML = `Conhecido como: Senhor Frio`
+        procCrime.innerHTML = `Procurado desde: 01/05/2019`
+    } else if (page == 2) {
+        nomeCrime.innerHTML = `Nome: Selina Kyle`
+        idadeCrime.innerHTML = `Idade: 28`
+        akaCrime.innerHTML = `Conhecida como: Mulher Gato`
+        procCrime.innerHTML = `Procurada desde: 17/09/2021`
+        criminoso.src = `main/imgs/selina.jpg`
+    } else if (page == 3) {
+        nomeCrime.innerHTML = `Nome: Bane Dorrance`
+        idadeCrime.innerHTML = `Idade: 30`
+        akaCrime.innerHTML = `Conhecido como: Bane`
+        procCrime.innerHTML = `Procurado desde: 21/11/2021`
+        criminoso.src = `main/imgs/bane.jpeg`
+    } else if (page == 4) {
+        nomeCrime.innerHTML = `Nome: Edward Nigma`
+        idadeCrime.innerHTML = `Idade: 34`
+        akaCrime.innerHTML = `Conhecido como: Charada`
+        procCrime.innerHTML = `Procurado desde: 21/11/2021`
+        criminoso.src = `main/imgs/nigma.jpg`
+    } else if (page == 5) {
+        page = 1
+        nomeCrime.innerHTML = `Nome: Victor Fries`
+        idadeCrime.innerHTML = `Idade: 47`
+        akaCrime.innerHTML = `Conhecido como: Senhor Frio`
+        procCrime.innerHTML = `Procurado desde: 01/05/2019`
+        criminoso.src = `main/imgs/fries.jpeg`
+    }
 }
